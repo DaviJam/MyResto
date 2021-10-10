98ee82d03aaa942a2ca7f15eab0d11c4b2ef5544
David James <daori971@gmail.com>
2021-10-10 21:10:49 +0200

    Comply with jdk8

--- MyResto-service/src/main/java/eu/ensup/myresto/service/LoggerService.java
+++ MyResto-service/src/main/java/eu/ensup/myresto/service/LoggerService.java
@@ -2,7 +2,6 @@ package eu.ensup.myresto.service;
 
 
 import eu.ensup.myresto.logger.LoggerHandler;
-import eu.ensup.myresto.service.IService;
 import org.apache.log4j.Logger;
 
 public class LoggerService extends LoggerHandler {
@@ -11,10 +10,10 @@ public class LoggerService extends LoggerHandler {
     }
 
     public void logServiceError(String className, String methodName, String message) {
-        Logger.getLogger(IService.class.getPackageName()).error("SERVICE Error: "+ className + " " + methodName + " - " + message);
+        Logger.getLogger(IService.class.getSimpleName()).error("SERVICE Error: "+ className + " " + methodName + " - " + message);
     }
 
     public void logServiceInfo(String className, String methodName, String message) {
-        Logger.getLogger(IService.class.getPackageName()).info("SERVICE Info: "+ className + " " + methodName + " - " + message);
+        Logger.getLogger(IService.class.getSimpleName()).info("SERVICE Info: "+ className + " " + methodName + " - " + message);
     }
 }
